8a519f9a44847e3385d63717a734cf5d86a41708
Jiri Kastan <djgeorge@seznam.cz>
2026-02-22 17:36:33 +0100

    Add My Release 6.8.0.30

--- Publish.bat
+++ Publish.bat
@@ -1,2 +1,10 @@
 @echo off
-dotnet publish SourceCode/AgOpenGPS.sln
+rem Property:Version  je pak verze AgIO
+rem cti REadme
+rem dotnet publish --no-restore --configuration ${{env.BUILD_CONFIGURATION}} --runtime ${{env.PUBLISH_RUNTIME}} ${{env.SOLUTION_FILE_PATH}}
+
+rem dotnet publish --no-restore --configuration Release --runtime win-x64 SourceCode/AgOpenGPS.sln /property:Version="2.3.0.5"
+
+dotnet build --no-restore SourceCode/AgOpenGPS.sln
+
+dotnet publish --no-restore --configuration Release SourceCode/AgOpenGPS.sln --property:Version="6.8.0.30"
